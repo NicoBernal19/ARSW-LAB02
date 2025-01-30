f0a6f530080196cad2b9c1db2958cfd870aea567
JuanPabl07DP <juanpadazer15@gmail.com>
2025-01-30 08:13:37 -0500

    Falta añadir método en Control

---
+++ src/main/java/primeFinderPackage/PrimeFinderThread.java
@@ -0,0 +1,56 @@
+package primeFinderPackage;
+/**
+ *
+ * @author Juan Pablo Daza Pereira
+ * @author Nicolas Bernal Fuquene
+ */
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PrimeFinderThread extends Thread{
+
+    int a,b;
+    private List<Integer> primes;
+    private Control control;
+
+    public PrimeFinderThread(int a, int b, Control control) {
+        super();
+        this.primes = new LinkedList<>();
+        this.a = a;
+        this.b = b;
+        this.control = control;
+    }
+
+    @Override
+    public void run(){
+        for (int i= a;i < b;i++){
+            try {
+                control.checkPaused();
+                if (isPrime(i)){
+                    primes.add(i);
+                    System.out.println(i);
+                }
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    boolean isPrime(int n) {
+        boolean ans;
+        if (n > 2) {
+            ans = n%2 != 0;
+            for(int i = 3;ans && i*i <= n; i+=2 ) {
+                ans = n % i != 0;
+            }
+        } else {
+            ans = n == 2;
+        }
+        return ans;
+    }
+
+    public List<Integer> getPrimes() {
+        return primes;
+    }
+}
