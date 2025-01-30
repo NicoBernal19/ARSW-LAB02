f994745ae91f386e4aa93124335db76fba0a9bb9
JuanPabl07DP <juanpadazer15@gmail.com>
2025-01-30 07:58:09 -0500

    Main hecho

---
+++ src/main/java/primeFinderPackage/Main.java
@@ -0,0 +1,16 @@
+package primeFinderPackage;
+
+/**
+ * @author Juan Pablo Daza Pereira
+ * @author Nicolas Bernal Fuquene
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Control control = Control.newControl();
+
+        control.start();
+
+    }
+
+}
